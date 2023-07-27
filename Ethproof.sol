b3e7d212406892f64763c4201d8a13712126a48d
Priya2444 <137447104+Priya2444@users.noreply.github.com>
2023-07-27 12:46:09 +0530

    Ethproof.sol

--- Ethproof.sol
+++ Ethproof.sol
@@ -16,25 +16,36 @@ pragma solidity 0.8.18;
 */
 
 contract MyToken {
+    // Public variables to store token details
+    string public tokenName;
+    string public tokenAbbrv;
+    uint public tokenSupply;
+    mapping(address => uint) public balances;
 
-    // public variables here
-    string public tokenName = "META";
-    string public tokenAbbrv = "MET";
-    uint public totalSupply = 0;
-
-    // mapping variable here
-    mapping (address => uint) public balances;
-    // mint function
-    function mint (address _address, uint _value) public {
-        totalSupply += _value;
-        balances [_address] += _value;    
+    constructor() {
+        // Initialize token details
+        tokenName = "Priya";
+        tokenAbbrv = "Pri";
+        tokenSupply = 1000000;
     }
 
-    // burn function
-    function bur (address _address, uint _value) public {
-        if (balances[_address] >= _value){
-        totalSupply -= _value;
-        balances [_address] -= _value;  
-        }   
+    function mint(address _address, uint _value) public {
+        // Increase the balance of the specified address
+        balances[_address] += _value;
+        // Increase the token supply
+        tokenSupply += _value;
+    }
+
+    function burn(address _address, uint _value) public returns (string memory) {
+        // Check if the address has sufficient balance to burn
+        require(balances[_address] >= _value, "Insufficient balance to burn");
+
+        // Deduct the specified value from the balance
+        balances[_address] -= _value;
+        // Deduct the specified value from the token supply
+        tokenSupply -= _value;
+
+        // Return success message
+        return "Tokens burned successfully";
     }
 }
